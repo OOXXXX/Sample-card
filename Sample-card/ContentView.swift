866a82665b0c748289fb481bdc4ac7da43108cd0
OOXXXX <zhupx2003@126.com>
2020-03-02 10:53:28 +0800

    Initial Commit

---
+++ Sample-card/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sample-card
+//
+//  Created by Rhapsody on 2020/3/2.
+//  Copyright © 2020 Rhapsody. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
