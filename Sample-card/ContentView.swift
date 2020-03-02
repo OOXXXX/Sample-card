67ea520e66a96861d97883023e34864ad113d254
OOXXXX <zhupx2003@126.com>
2020-03-02 17:17:00 +0800

    Second Commit (initialize)

--- Sample-card/ContentView.swift
+++ Sample-card/ContentView.swift
@@ -10,12 +10,125 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            AboutTitleView()
+            
+            
+            BackCardView()
+                .background(Color("card4"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: -40.0)
+                .scaleEffect(0.9)
+                .rotationEffect(.degrees(10))
+                .rotation3DEffect(Angle(degrees: 10.0), axis: (x: 10.0, y: 0.0, z: 0.0))
+                .blendMode(.hardLight)
+            
+            BackCardView()
+                .background(Color("card3"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: -20.0)
+                .scaleEffect(0.95)
+                .rotationEffect(.degrees(5))
+                .rotation3DEffect(Angle(degrees: 5.0), axis: (x: 10.0, y: 0.0, z: 0.0))
+                .blendMode(.hardLight)
+            
+            ForeCard()
+            
+            BottomCardView()
+            
+        }
+    }
+}
+
+struct ForeCard: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Programming")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                    Text("Swift / Python / CSS")
+                        .foregroundColor(Color("accent"))
+                }
+                
+                Spacer()
+                Image("Logo1")
+            }
+            .padding()
+            Spacer()
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 110, alignment: .top)
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        .frame(width: 340.0, height: 220.0)
+        
+    }
+}
+
+struct AboutTitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("About")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding(.horizontal)
+            .padding(5)
+            Image("Background1")
+                .padding(.top, -18.0)
+            Spacer()
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct BottomCardView: View {
+    var body: some View {
+        VStack {
+            Rectangle()
+                .frame(width: 40, height: 5)
+                .cornerRadius(3)
+                .opacity(0.1)
+            
+            
+            Text("One of my favorite hobbies is Programming. I always believe me as a developer and that’s also what I dream to be. I am interested in Swift, Python and some Front-end web development, including CSS, HTML and JavaScript.")
+                
+                .fontWeight(.medium)
+                .multilineTextAlignment(.leading)
+                .font(.custom("arial", size: 18))
+                .padding(.top, 10.0)
+                .lineSpacing(5)
+            Spacer()
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .offset(x: 0, y: 500)
     }
 }
